d007d4d32fb2e84d60c9ff28a7ccef98966eb914
Gregory Salinas <asalinas720@gmail.com>
2025-02-09 19:57:05 -0800

    chore: setup scripts commands

--- scripts/src/builds/themes.ts
+++ scripts/src/builds/themes.ts
@@ -1,7 +1,17 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import type { SpawnOptions } from "bun";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const opts: SpawnOptions.OptionsObject = {
+  cwd: join(__dirname, "..", "..", "..", "./themes")
+};
+
 const main = async () => {
-  Bun.spawn(["bun", "build:minimal"]);
-  Bun.spawn(["bun", "build:park"]);
-  Bun.spawn(["bun", "build:shadcn"]);
+  Bun.spawn(["bun", "minimal"], opts);
+  Bun.spawn(["bun", "park"], opts);
+  Bun.spawn(["bun", "shadcn"], opts);
 };
 
 main();
