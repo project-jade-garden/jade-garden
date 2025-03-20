a15ee9fa12f07ba3481e2a913dbdeb33c99f45f6
Gregory Salinas <asalinas720@gmail.com>
2025-03-20 01:29:37 -0700

    chore(libs): Ark UI - update tests

--- libs/ark-ui/tests/anatomy.test.ts
+++ libs/ark-ui/tests/anatomy.test.ts
@@ -6,23 +6,9 @@ import { components as dataComponents } from "./data";
 const components = Object.keys(dataComponents);
 const customArkComponents = ["colorPicker", "checkbox", "field", "fieldset", "segmentGroup", "toggle", "tour"];
 
-describe("@zag-js/anatomy@1.2.1", () => {
-  test.each(customArkComponents)("'%s' matches matches current anatomy of Ark UI", async (component) => {
-    // @ts-expect-error: Element implicitly has an 'any' type because expression of type '`${string}Slots`' can't be used to index type 'typeof srcComponents'.
-    const srcSlots = srcComponents[`${camelCase(component)}Slots`];
-    // @ts-expect-error: Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'components'.
-    const dataSlots = dataComponents[component];
-
-    if (typeof dataSlots === "object" && Array.isArray(dataSlots)) {
-      expect(dataSlots).toContainAllValues(srcSlots);
-    } else {
-      expect(dataSlots).toEqual(srcSlots);
-    }
-  });
-
-  test.each(components.filter((component) => !customArkComponents.includes(component)))(
-    "'%s' matches current anatomy of @zag-js",
-    async (component) => {
+describe("@ark-ui/*@5.x", () => {
+  describe("Ark UI anatomy", () => {
+    test.each(customArkComponents)("'%s' matches current anatomy", async (component) => {
       // @ts-expect-error: Element implicitly has an 'any' type because expression of type '`${string}Slots`' can't be used to index type 'typeof srcComponents'.
       const srcSlots = srcComponents[`${camelCase(component)}Slots`];
       // @ts-expect-error: Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'components'.
@@ -33,6 +19,24 @@ describe("@zag-js/anatomy@1.2.1", () => {
       } else {
         expect(dataSlots).toEqual(srcSlots);
       }
-    }
-  );
+    });
+  });
+
+  describe("@zag-js/anatomy@1.2.1", () => {
+    test.each(components.filter((component) => !customArkComponents.includes(component)))(
+      "'%s' matches current anatomy of @zag-js",
+      async (component) => {
+        // @ts-expect-error: Element implicitly has an 'any' type because expression of type '`${string}Slots`' can't be used to index type 'typeof srcComponents'.
+        const srcSlots = srcComponents[`${camelCase(component)}Slots`];
+        // @ts-expect-error: Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'components'.
+        const dataSlots = dataComponents[component];
+
+        if (typeof dataSlots === "object" && Array.isArray(dataSlots)) {
+          expect(dataSlots).toContainAllValues(srcSlots);
+        } else {
+          expect(dataSlots).toEqual(srcSlots);
+        }
+      }
+    );
+  });
 });
