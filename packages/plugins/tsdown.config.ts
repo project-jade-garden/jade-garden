dd1fb40000d8cac8af4a19e8f8b8e41f254de2b0
Gregory Salinas <asalinas720@gmail.com>
2025-09-13 21:36:51 -0700

    refactor(unplugin-jade-garden): switch to tsdown

--- packages/plugins/tsdown.config.ts
+++ packages/plugins/tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./src/index.ts", "./src/rollup.ts", "./src/rspack.ts", "./src/vite.ts", "./src/webpack.ts"],
+  entry: ["src/*.ts"],
   format: ["cjs", "esm"],
   target: "es2022",
   minify: true,
